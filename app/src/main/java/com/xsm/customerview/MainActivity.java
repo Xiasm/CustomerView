e69fb218dde8cd17a83b6d797b95edc0fa2ef581
xiashengming <xiasem@163.com>
2018-03-09 15:23:03 +0800

    自定义iew--计时器V

---
+++ app/src/main/java/com/xsm/customerview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.xsm.customerview;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.xsm.customerview.widgets.CountdownView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CountdownView mCountdownView;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    number--;
+                    if (number > 0) {
+                        mCountdownView.setCurrentProgress(number);
+                        mHandler.sendEmptyMessageDelayed(1, 1000);
+                    } else {
+                        mCountdownView.setCurrentProgress(0);
+                    }
+                    break;
+            }
+        }
+    };
+
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCountdownView = findViewById(R.id.countdownView);
+        number = 60;
+        mCountdownView.setTotalProgress(60);
+        mHandler.sendEmptyMessageDelayed(1, 2000);
+    }
+
+
+}
